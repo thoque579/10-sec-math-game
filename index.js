d1600e538fc44e627bd6dbc7b5a16b16b2ec55d0
thoque579 <tusharhoque2@gmail.com>
2021-04-06 14:24:32 -0700

    added timer and correct answer function

--- index.js
+++ index.js
@@ -1,53 +1,66 @@
+$(document).ready(function()
+  {
+    var currentQuestion;
+    var randomNumberGenerator = function (size)
+    {
+      return Math.ceil(Math.random() * size);
+    }
 
-$(document).ready(function () {
-  var currentQuestion;
-
-  var randomNumberGenerator = function (size) {
-    return Math.ceil(Math.random() * size);
-  }
-
-  var questionGenerator = function () {
+  var questionGenerator = function ()
+  {
     var question = {};
     var num1 = randomNumberGenerator(10);
     var num2 = randomNumberGenerator(10);
 
     question.answer = num1 + num2;
-    question.equation = String(num1) + "+" + String(num2);
+    question.equation = String(num1) + " + " + String(num2);
 
     return question;
   }
 
-  console.log(questionGenerator());
-  console.log(questionGenerator());
+  var renderNewQuestion = function ()
+  {
+    currentQuestion = questionGenerator();
+    $('#equation').text(currentQuestion.equation);
+  }
 
-  currentQuestion = questionGenerator();
-
-  $('#equation').text(currentQuestion.equation);
-
-  var checkAnswer = function (userInput, answer) {
-    console.log(userInput === answer);
+  var checkAnswer = function (userInput, answer)
+  {
+    if (userInput === answer)
+    {
+      renderNewQuestion();
+      $('#user-input').val('');
+      updateTimeLeft(+1);
+    }
   }
 
   $('#user-input').on('keyup', function ()
   {
-    console.log($(this).val(), currentQuestion.answer);
+    checkAnswer(Number($(this).val()), currentQuestion.answer);
   });
 
-  var renderNewQuestion = function ()
-  {
-    currentQuestion = questionGenerator;
-    $('#equation').text(currentQuestion);
-  }
-
-  var checkAnswer = function (userInput, answer) {
-    if (userInput === answer)
-    {
-      renderNewQuestion();
-    }
-  }
-
-  $('#user-input').on('keyup', function () {
-    checkAnswer(Number($(this).value()), currentQuestion.answer);
-  });
   renderNewQuestion();
-})
+// amount of time left the user has
+  var timeLeft = 10;
+  setInterval(function ()
+  {
+    console.log('1 sec passed');
+  },1000);
+
+  var interval = setInterval(function ()
+  {
+    timeLeft--;
+    $('#time-left').text(timeLeft);
+    if (timeLeft === 0)
+    {
+      clearInterval(interval);
+    }
+    console.log(timeLeft);
+  } ,1000);
+
+  var updateTimeLeft = function(amount)
+  {
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  }
+});
